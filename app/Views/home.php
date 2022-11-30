6e4935fab91699ca5361d1c63c7115d74441e028
DESKTOP-RUL295T\adity <adityafjri@gmail.com>
2022-11-30 21:01:47 +0700

    rapihin navigasi

--- app/Views/home.php
+++ app/Views/home.php
@@ -4,13 +4,16 @@
     <link rel="stylesheet" href="../assets/css/style.css">
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
     <?= view('template/header.php', ['css_dependencies' => ['']]) ?>
 </head>
 
 <body class="">
     <!-- popup -->
     <div class="title-bar">
-        <div id="title">MovieKu</div>
+        <h3 id="title">MovieKu</h3>
         <ul>
             <li><a class="active" href="#home">Home</a></li>
             <li><a href="#news">Trending</a></li>

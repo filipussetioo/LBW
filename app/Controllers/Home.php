9451e77aaee6ad79fdc12ef60b1ac03827b6698f
pndevworks_deploy <filipus54@gmail.com>
2022-12-08 07:34:25 +0700

    api series

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,15 +6,19 @@ class Home extends BaseController
 {
     public function index()
     {
-        $readApi = file_get_contents('https://api.themoviedb.org/3/movie/2/recommendations?api_key=4d039461c194e3b4f6c776c5cd99d7c1&language=en-US&page=1');
+        $readApi = file_get_contents('https://api.themoviedb.org/3/movie/popular?api_key=4d039461c194e3b4f6c776c5cd99d7c1&language=en-US&page=1');
 
         $data = json_decode($readApi,true);
+
+        $apiSeries = file_get_contents('https://api.themoviedb.org/3/tv/popular?api_key=4d039461c194e3b4f6c776c5cd99d7c1&language=en-US&page=1');
+        $dataSeries = json_decode($apiSeries,true);
         // $data_array = array(
         //     'datalist' => $data
         // );
 
         return view('home.php',[
             'data' => $data,
+            'dataSeries' => $dataSeries
         ]); 
         
     }

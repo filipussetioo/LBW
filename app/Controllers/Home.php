076f88c7125c21edd62d7360eb6f318abdc5c0e8
pndevworks_deploy <filipus54@gmail.com>
2022-12-20 02:17:07 +0700

    login still error

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -39,6 +39,52 @@ class Home extends BaseController
     }
     public function process()
     {
+        if (!$this->validate([
+            'username' => [
+                'rules' => 'required|min_length[4]|max_length[20]|is_unique[users.username]',
+                'errors' => [
+                    'required' => '{field} Harus diisi',
+                    'min_length' => '{field} Minimal 4 Karakter',
+                    'max_length' => '{field} Maksimal 20 Karakter',
+                    'is_unique' => 'Username sudah digunakan sebelumnya'
+                ]
+            ],
+            'password' => [
+                'rules' => 'required|min_length[4]|max_length[50]',
+                'errors' => [
+                    'required' => '{field} Harus diisi',
+                    'min_length' => '{field} Minimal 4 Karakter',
+                    'max_length' => '{field} Maksimal 50 Karakter',
+                ]
+            ],
+            'password_conf' => [
+                'rules' => 'matches[password]',
+                'errors' => [
+                    'matches' => 'Konfirmasi Password tidak sesuai dengan password',
+                ]
+            ],
+            'name' => [
+                'rules' => 'required|min_length[4]|max_length[100]',
+                'errors' => [
+                    'required' => '{field} Harus diisi',
+                    'min_length' => '{field} Minimal 4 Karakter',
+                    'max_length' => '{field} Maksimal 100 Karakter',
+                ]
+            ],
+        ])) {
+            session()->setFlashdata('error', $this->validator->listErrors());
+            return redirect()->back()->withInput();
+        }
+        $users = new Users();
+        $users->insert([
+            'username' => $this->request->getVar('username'),
+            'password' => password_hash($this->request->getVar('password'), PASSWORD_BCRYPT),
+            'name' => $this->request->getVar('name')
+        ]);
+        return redirect()->to('/');
+    }
+    public function login()
+    {
         $users = new Users();
         $username = $this->request->getVar('username');
         $password = $this->request->getVar('password');

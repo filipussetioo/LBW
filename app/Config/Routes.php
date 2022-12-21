4e5285368c2fbbc6933357a002e4f42ef5677bcf
pndevworks_deploy <filipus54@gmail.com>
2022-12-21 16:02:19 +0700

    logout

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -40,7 +40,7 @@ $routes->get('/home2', 'Home2::index');
 $routes->get('/register', 'Register::index');
 $routes->post('/register/process', 'Register::process');
 $routes->get('/watchlist', 'Watchlist::index');
-$routes->post('/home/process', 'Home::process');
+$routes->get('/home/logout', 'Home::logout');
 $routes->post('/home/login', 'Home::login');
 
 

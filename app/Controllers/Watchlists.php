d7faf9e7eacaffd4e06af54422de0882b9b07d1d
pndevworks_deploy <filipus54@gmail.com>
2023-01-05 00:54:33 +0700

    deleting unused code

--- app/Controllers/Watchlists.php
+++ app/Controllers/Watchlists.php
@@ -37,9 +37,6 @@ class Watchlists extends BaseController
         $watchlist_streaming_data = $builder_streaming->get()->getResult();
             if($watchlist_streaming_data[0]->streaming_platform == 'null'){
                 $watchlist_streaming_data = $watchlist_streaming_data[1];
-            }
-            else{
-                $watchlist_streaming_data = $watchlist_streaming_data;
             };
         return view('watchlist.php',[
             'data' => $data,
